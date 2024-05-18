70cd778a1a199226fc40ca721c45f8f1ffc74941
Carlos Chavez <78452524+chavezcarlos00@users.noreply.github.com>
2024-05-17 20:39:01 -0600

    Update conexion.php

--- logica/conexion.php
+++ logica/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host_db="127.0.0.1:3306";
-$user_db="root";
-$pass_db="";
-$name_db="ProyectoSI";
+$host_db="192.168.0.13:3306";
+$user_db="carlosmarin";
+$pass_db="123456";
+$name_db="informacion";
 
 $conexion = new mysqli($host_db,$user_db,$pass_db,$name_db);
 
